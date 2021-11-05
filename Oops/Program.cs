6ffcbaa0b575daa69e1550d41a60fd3665b7889d
uni2tw@gmail.com <uni2tw@gmail.com>
2021-11-05 11:40:25 +0800

    可依EnvironmentName多讀取appsettings

--- Oops/Program.cs
+++ Oops/Program.cs
@@ -1,15 +1,7 @@
 ﻿using System;
-using System.Data.SQLite;
-using System.IO;
-using System.Reflection.Metadata;
-using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using Oops.Components;
 using Oops.Daos;
@@ -22,41 +14,12 @@ namespace Oops
     {
         static void Main(string[] args)
         {
-            if (args.Length > 0)
-            {
-                Helper.SetRoot(args[0].Trim('\'', '"'));
-            }
+            //if (args.Length > 0)
+            //{
+            //    Helper.SetRoot(args[0].Trim('\'', '"'));
+            //}
 
             Console.WriteLine(Helper.MapPath("assets"));
-
-            IoC.Register();
-
-            IConfigurationRoot configurationRoot = new ConfigurationBuilder()
-                .AddJsonFile(Helper.MapPath("appsettings.json"), false)
-                .AddEnvironmentVariables()
-                .Build();            
-            IoC.Get<IDBConnectionConfig>().ConnectionString = configurationRoot.GetSection("DBSetting:ConnectionString").Value;
-
-            ErrorDao dao = IoC.Get<ErrorDao>();
-            Console.WriteLine("LoadConnectString: " + dao.LoadConnectString());
-            DbUtil.EnsureDBFile(dao.LoadConnectString());
-            DbUtil.EnsureTable<Error>(dao.LoadConnectString());
-            DbUtil.EnsureIndex<Error>(dao.LoadConnectString(), "Time");
-            DbUtil.EnsureIndex<Error>(dao.LoadConnectString(), "Application", "Time");
-
-            DbUtil.EnsureTable<OopsLog>(dao.LoadConnectString());           
-
-            DbUtil.EnsureIndex<OopsLog>(dao.LoadConnectString(), nameof(OopsLog.Date), nameof(OopsLog.Srv), nameof(OopsLog.Logger));
-            DbUtil.EnsureIndex<OopsLog>(dao.LoadConnectString(), nameof(OopsLog.Date), nameof(OopsLog.Logger), nameof(OopsLog.Srv));
-            DbUtil.EnsureIndex<OopsLog>(dao.LoadConnectString(), nameof(OopsLog.Srv), nameof(OopsLog.Date));
-            DbUtil.EnsureIndex<OopsLog>(dao.LoadConnectString(), nameof(OopsLog.Srv), nameof(OopsLog.Level), nameof(OopsLog.Date));
-            DbUtil.EnsureIndex<OopsLog>(dao.LoadConnectString(), nameof(OopsLog.Srv), nameof(OopsLog.Logger), nameof(OopsLog.Level), nameof(OopsLog.Date));
-            DbUtil.EnsureIndex<OopsLog>(dao.LoadConnectString(), nameof(OopsLog.Srv), nameof(OopsLog.Logger), nameof(OopsLog.Date));            
-            DbUtil.EnsureIndex<OopsLog>(dao.LoadConnectString(), nameof(OopsLog.Logger), nameof(OopsLog.Date));
-            DbUtil.EnsureIndex<OopsLog>(dao.LoadConnectString(), nameof(OopsLog.Logger), nameof(OopsLog.Level), nameof(OopsLog.Date));
-            DbUtil.EnsureIndex<OopsLog>(dao.LoadConnectString(), nameof(OopsLog.Logger), nameof(OopsLog.Srv), nameof(OopsLog.Date));
-
-            IoC.Get<IMqttService>().Start();
 
             var host = Host.CreateDefaultBuilder()                
                 .ConfigureWebHost(cfg =>
@@ -70,6 +33,37 @@ namespace Oops
                 .ConfigureAppConfiguration((ctx, cfg) =>
                 {
                     var env = ctx.HostingEnvironment;
+
+                    IoC.Register();
+
+                    IConfigurationRoot configurationRoot = new ConfigurationBuilder()
+                        .AddJsonFile(Helper.MapPath("appsettings.json"), false)
+                        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, true)
+                        .AddEnvironmentVariables()
+                        .Build();
+                    IoC.Get<IDBConnectionConfig>().ConnectionString = configurationRoot.GetSection("DBSetting:ConnectionString").Value;
+
+                    ErrorDao dao = IoC.Get<ErrorDao>();
+                    Console.WriteLine("LoadConnectString: " + dao.LoadConnectString());
+                    DbUtil.EnsureDBFile(dao.LoadConnectString());
+                    DbUtil.EnsureTable<Error>(dao.LoadConnectString());
+                    DbUtil.EnsureIndex<Error>(dao.LoadConnectString(), "Time");
+                    DbUtil.EnsureIndex<Error>(dao.LoadConnectString(), "Application", "Time");
+
+                    DbUtil.EnsureTable<OopsLog>(dao.LoadConnectString());
+
+                    DbUtil.EnsureIndex<OopsLog>(dao.LoadConnectString(), nameof(OopsLog.Date), nameof(OopsLog.Srv), nameof(OopsLog.Logger));
+                    DbUtil.EnsureIndex<OopsLog>(dao.LoadConnectString(), nameof(OopsLog.Date), nameof(OopsLog.Logger), nameof(OopsLog.Srv));
+                    DbUtil.EnsureIndex<OopsLog>(dao.LoadConnectString(), nameof(OopsLog.Srv), nameof(OopsLog.Date));
+                    DbUtil.EnsureIndex<OopsLog>(dao.LoadConnectString(), nameof(OopsLog.Srv), nameof(OopsLog.Level), nameof(OopsLog.Date));
+                    DbUtil.EnsureIndex<OopsLog>(dao.LoadConnectString(), nameof(OopsLog.Srv), nameof(OopsLog.Logger), nameof(OopsLog.Level), nameof(OopsLog.Date));
+                    DbUtil.EnsureIndex<OopsLog>(dao.LoadConnectString(), nameof(OopsLog.Srv), nameof(OopsLog.Logger), nameof(OopsLog.Date));
+                    DbUtil.EnsureIndex<OopsLog>(dao.LoadConnectString(), nameof(OopsLog.Logger), nameof(OopsLog.Date));
+                    DbUtil.EnsureIndex<OopsLog>(dao.LoadConnectString(), nameof(OopsLog.Logger), nameof(OopsLog.Level), nameof(OopsLog.Date));
+                    DbUtil.EnsureIndex<OopsLog>(dao.LoadConnectString(), nameof(OopsLog.Logger), nameof(OopsLog.Srv), nameof(OopsLog.Date));
+
+                    IoC.Get<IMqttService>().Start();
+
                     cfg.AddConfiguration(configurationRoot);
                 })
                 .Build();
